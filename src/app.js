b0c04316f6fc5e190adbc9c5ad34d8ab91850b15
Jimmbo19 <jega190112@gmail.com>
2024-08-08 01:26:56 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domains = [".com", ".net", ".us", ".io", ".etc"];
+
+let combinations = [];
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let d of domains) {
+        combinations.push(`${p} ${a} ${n}${d}`);
+      }
+    }
+  }
+}
+
+combinations.forEach(combination => {
+  console.log(combinations);
+});
